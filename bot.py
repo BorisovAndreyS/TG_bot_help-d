0c5b0e0a69f792b2116bb68b453585b004aeeacc
helmut <helmut.andrey@gmail.com>
2024-10-31 13:46:43 +0500

    Task4. Готово!!!!

--- bot.py
+++ bot.py
@@ -16,8 +16,9 @@ import logging
 #     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 # )
 
-# Определяем состояния для разных меню
-MENU_MAIN, MENU_ECHO, MENU_RANDOM, MENU_GPT, MENU_TALK = range(5)
+# Определяем состояния для разных меню #Меню выбора персонажа
+MENU_MAIN, MENU_ECHO, MENU_RANDOM, MENU_GPT, MENU_TALK, ONE, TWO, THREE, FOUR, FIVE = range(10)
+
 
 chat_gpt = ChatGptService(ChatGPT_TOKEN)
 
@@ -91,7 +92,7 @@ async def handle_random(update: Update, context: ContextTypes.DEFAULT_TYPE):
     # Загрузили Промпт
     prompt = load_prompt('random')
     # Отправляем картинку
-    # await send_image(update, context, 'random')
+    await send_image(update, context, 'random')
 
     # Получаем тект
     text = load_message('random')
@@ -144,29 +145,81 @@ async def handle_gpt(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 
-
+#Меню Talk, Диалог с известной личностью
 async def talk(update: Update, context: ContextTypes.DEFAULT_TYPE):
     #Вход в меню Talk
-    text = load_message('talk')
-    await send_text(update, context, text)
-    await send_image(update, context, 'talk_cobain')
+
+    text_talk = load_message('talk')
+    keyboard = [
+        [
+            InlineKeyboardButton("1", callback_data=str(ONE)),
+            InlineKeyboardButton("2", callback_data=str(TWO)),
+            InlineKeyboardButton("3", callback_data=str(THREE)),
+            InlineKeyboardButton("4", callback_data=str(FOUR)),
+            InlineKeyboardButton("5", callback_data=str(FIVE)),
+            InlineKeyboardButton("Выход", callback_data=str(MENU_MAIN)),
+
+        ]
+    ]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    await update.message.reply_text(text_talk, reply_markup=reply_markup)
+
+    return MENU_TALK
+
+star_human = {
+    '5': ['Курт Кобейн', 'talk_cobain'],
+    '6': ['Елизавета II', 'talk_queen'],
+    '7': ['Джон Толкиен', 'talk_tolkien'],
+    '8': ['Фридрих Ницше', 'talk_nietzsche'],
+    '9': ['Стивен Хокинг', 'talk_hawking']
+}
+
+
+
+async def talk_button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    #Обработка кнопки talk
+    query = update.callback_query
+    await query.answer()
+
+    select_key = query.data
+    if select_key == str(MENU_MAIN):
+        return await back_to_main()
+
+    select_star = star_human.get(select_key)
+    if not select_star:
+        await send_text(update, context, 'Выбран не верный персонаж')
+        return MENU_MAIN
+
+    await send_text(update, context, f'Отличный выбор, буду общаться с тобой как {star_human[query.data][0]} ! \n'
+                                     f'Что ты хочешь узнать?')
+
+    await send_image(update, context, select_star[1])
+    #Установка состояния выбора
+    context.user_data['select_star'] = select_star[1]
 
     return MENU_TALK
 
 
-async def handle_talk(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    #логиика работы раздела Talk
-    #
-    chat_gpt.set_prompt('talk_cobain')
+
+
+#Функция обработки вызова talk
+async def handle_talk(update: Update, context: ContextTypes.DEFAULT_TYPE, star_human_query = None):
+    select_prompt = context.user_data['select_star']
+    # print(select_prompt)
+    if not select_prompt:
+        await send_text(update, context, "Произошла ошибка. Пожалуйста, выберите снова.")
+        return MENU_TALK
+
+    chat_gpt.set_prompt(select_prompt)
     text = update.message.text
-    # answer = await chat_gpt.send_message_list()
     answer = await chat_gpt.add_message(text)
-    await send_text(update, context, answer)
+    await update.message.reply_text(answer)
     return MENU_TALK
 
 
-async def talk_cobain(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    pass
+
 
 
 #функция возврата в главное меню
@@ -196,7 +249,7 @@ def main():
             MENU_RANDOM: [
                 CommandHandler('random', handle_random),
                 CallbackQueryHandler(random_button_handler, pattern="^" + str(MENU_RANDOM) + "$"),
-                CallbackQueryHandler(random_button_handler, pattern="^" + str(MENU_MAIN) + "$"),
+                CallbackQueryHandler(back_to_main, pattern="^" + str(MENU_MAIN) + "$"),
                 CommandHandler('back', back_to_main)
             ],
             MENU_GPT: [
@@ -205,6 +258,7 @@ def main():
             ],
             MENU_TALK: [
                 MessageHandler(filters.TEXT & ~filters.COMMAND, handle_talk),
+                CallbackQueryHandler(talk_button_handler, pattern="^(5|6|7|8|9|MENU_MAIN)$"),
                 CommandHandler('back', back_to_main)
             ],
         },
